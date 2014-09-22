49d75ab02f7cf4cc382605bc50c3166f0679e6bd
ccl2of4 <drconnoramerica@utexas.edu>
2014-09-22 01:50:47 -0500

    Update fileIO.h

--- fileIO.h
+++ fileIO.h
@@ -1,6 +1,7 @@
 /*
   returns next line of input in file similar to Java's nextLine() method on Scanner
   returns NULL if there is no more input
+  caller of function is responsible for freeing memory allocated to the char * that is returned
 
 /*
 char *next_line(FILE *file);
